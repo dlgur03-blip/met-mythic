0cfd00fd230786b9bb2e6a4f046da3a28bf20441
dlgur03-blip <dlgur03@gmail.com>
2025-12-04 03:51:54 +0900

    fix: remove .tsx extension

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,7 @@ export { TestScreen } from './TestScreen';
 export { ResultScreen } from './ResultScreen';
 export { FullResultScreen } from './FullResultScreen';
 export { ReportViewer } from './ReportViewer';
-export { NicknameInput } from './NicknameInput.tsx';
+export { NicknameInput } from './NicknameInput';
 
 // Option Components
 export { ChoiceOptions } from './options/ChoiceOptions';
